b7077b0efadd9931387b17bd802c88176c4a359d
Alexey Tyulenev <leksey.tyulenev@gmail.com>
2023-02-20 23:28:27 +0300

    задание 2
    задание 2

---
+++ Ex2_/arrays/arrays.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void rotate(int a[], int n, bool clockwise) {
+	if (!clockwise) {
+		int x = a[0];
+		for (int i = 0; i < n - 1; i++) {
+			a[i] = a[i + 1];
+		}
+		a[n - 1] = x;
+	}
+	else {
+		int y = a[n - 1];
+		for (int i = 1; i < n; i++) {
+			a[n - i] = a[n - i - 1];
+		}
+		a[0] = y;
+	}
+}
+
+
+int main()
+{
+	int a[5]{ 1,2,3,4,5 };
+	/*for (int i = 0; i < 5; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+
+	for (int *p = a; p-a<5; p++)
+		cout << *p << ' ';
+	cout << endl;*/
+
+	rotate(a, 5, true);
+	rotate(a, 5, false);
+	rotate(a, 5, true);
+
+
+	for (int i = 0; i < 5; i++)
+		cout << a[i] << ' ';
+	cout << endl;
+}
---
+++ Ex2_/strings/strings.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+void string_copy(char *d, char* s) {
+	for (int i = 0; *d++ = *s++;);
+}
+
+int len(const char *s) {
+	int l;
+	for (int i = 0; s[i]; i++)
+		l = i;
+	return ++l;
+}
+
+int compare(const char* s, const char* t) {
+	cout << s << endl;
+	cout << t << endl;
+	for ((*t == *s) && *s; t++, s++;) {
+		return *s - *t;
+	}
+}
+
+int main()
+{
+	char str[] = "hello!";
+	cout << str << endl;
+	cout << strlen(str) << ' ' << sizeof(str) << endl;
+	char t[32];
+	char l[32] = "saod";
+	char* pl = l;
+	char* pd = t;
+	char* ps = str;
+	string_copy(pd, ps);
+
+	cout << len(ps) << endl;
+
+	cout << compare(ps, pl) << endl;
+}
